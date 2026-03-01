9bdea0e01056d0f9eff5f92aa7edb7bb13da031b
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2026-03-01 12:01:31 -0300

    feat: add MongoDB connection

---
+++ database.py
@@ -0,0 +1,15 @@
+#conexão com o banco
+
+import os
+from pymongo import MongoClient, ASCENDING
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+
+client = MongoClient(MONGO_URI)
+db = client['hacker_news_db']
+collection = db['posts']
+
+collection.create_index([("title", ASCENDING)], unique=True)
+collection.create_index([("country", ASCENDING)])
+collection.create_index([("domain", ASCENDING)])
+collection.create_index([("author", ASCENDING)])
